942ef655071b85452b7531b9406588f9a441d37a
Droxl <Droxl@users.noreply.github.com>
2017-11-06 10:25:13 +0100

    Update Texttrenner.java

--- Praktikumsaufgabe 09/Texttrenner.java
+++ Praktikumsaufgabe 09/Texttrenner.java
@@ -1,44 +1,11 @@
+package ungeloest;
 /**
- * Der Texttrenner erlaubt es einer Anwendung, eine Zeichenkette in 
- * Teilzeichenketten (Wörter genannt), aufzuteilen.
- * <p>
- * Die Menge der Trennzeichen (Zeichen die die Wörter aufteilen), müssen 
- * bei der Initialisierung festgelegt werden.
- * <p>
- * Ein <tt>Texttrenner</tt> -Objekt 
- * unterhält intern eine momentane Position innerhalb der Zeichenkette, 
- * die in Wörter unterteilt wird. 
- * Einige Operationen erhöhen die momentane Position über die Zeichen.
- * </p>
- * 
- * Ein Wort wird dadurch wiedergegeben, dass eine Teilkette der übergebenen 
- * Zeichenkette entnommen wird. Die erste Zeichenkette, die genutzt wurde, 
- * um das <tt>Texttrenner</tt> -Objekt zu erstellen.
- * </p>
- * Das folgende Beispiel zeigt den Gebrauch des Texttrenners. 
- * Code:
- * <blockquote><pre>
- *     Texttrenner zeichenkette = new Texttrenner("Dies ist ein Test");
- *     while (zeichenkette.hatNochWoerter()) {
- *         System.out.println(zeichenkette.gibNaechstesWort());
- *     }
- * </pre></blockquote>
- * <p>
- * Führt zu folgender Ausgabe:
- * <blockquote><pre>
- *     Dies
- *     ist
- *     ein
- *     Test
- * </pre></blockquote></p>
- * @see     java.io.StreamTokenizer
- * @since   JDK1.0
- * @see     Siehe dazu auch: http://grepcode.com/file_/repository.grepcode.com
- * /java/root/jdk/openjdk/6-b14/java/util/StringTokenizer.java/?v=source*/
-/**
- * Diese Klasse trennt Wörter einer Zeichenkette, anhand einer gegebenen 
+ * Diese Klasse trennt Wörter einer Zeichenkette anhand einer gegebenen 
  * zweiten Zeichenkette, welche nur aus Trennzeichen besteht.
- * @author Droxl
+ * Allgemeine Funktionsweise siehe:
+ * http://grepcode.com/file_/repository.grepcode.com/java/root/jdk/
+ * openjdk/6-b14/java/util/StringTokenizer.java/?v=source
+ * Der Texttrenner gilt allerdings als überholt!
  */
 public class Texttrenner {
     /**
@@ -46,12 +13,12 @@ public class Texttrenner {
      */
     private final String zeichenkette;
     /**
-     * Nur aus Trennzeichen bestehende Zeichenkette, mit der operiert wird.
+     * Nur aus Trennzeichen bestehende Zeichenkette mit der operiert wird.
      */
     private final String trennerkette;
     /**
-     * Die jeweils aktuelle Position. 
-     * Der "Pfeil" steht jeweils am Anfang des nächsten auszugebenden Wortes.
+     * Die jeweils aktuelle Position.
+     * Der Pfeil steht jeweils auf am Anfang des nächsten auszugebenden Wortes.
      */
     private int position;
     /**
@@ -62,7 +29,7 @@ public class Texttrenner {
      * Erzeugt ein Texttrenner-Objekt mit einer Zeichenkette 
      * und einer Trennerkette.
      * @param zeichenkette Gesamte Zeichenkette auf der operiert wird.
-     * @param trennerkette Nur aus Trennzeichen bestehende Zeichenkette, 
+     * @param trennerkette Nur aus Trennzeichen bestehende Zeichenkette 
      * mit der operiert wird.
      */
     public Texttrenner(String zeichenkette, String trennerkette) {
@@ -73,7 +40,7 @@ public class Texttrenner {
         maxPosition = zeichenkette.length();
         /*Übergabe des Trennzeichenkettenparameters zur Initialisierung.*/
         this.trennerkette = trennerkette;
-        //Der "Pfeil" steht immer am Anfang des nächsten, auszugebenden Wortes.
+        //Der "Pfeil" steht immer am Anfang des nächsten auszugebenden Wortes.
         position = anfangWort();
     }
     /**
@@ -84,26 +51,24 @@ public class Texttrenner {
     private boolean istTrennzeichen() {
         //Ein Zeichen, das angibt, ob ein Trennzeichen vorliegt.
         int semaphor = 0;
-        /*Indikator, ob das übergebene Zeichen tatsächlich 
-        ein Trennzeichen ist.*/
+        //Indikator, ob das übergebene Zeichen ein Trennzeichen ist.
         boolean istTrennzeichen = false;
         //Variable zur Bestimmung der Position des Trennzeichens.
         int trennzeichenAnPosition = 0;
         /*Eine Schleife, die die Position (also den "Pfeil"), 
-        über die Zeichenkette iterieren lässt, und 
+        über die Zeichenkette iterieren lässt; und 
         das Trennzeichen (Trennzeichen an Position) über die Trennerkette.*/
         while (trennzeichenAnPosition < trennerkette.length() 
                 && position < zeichenkette.length()) {
             /*Stimmen das Zeichen der Zeichenkette und das Trennzeichen 
-            der Trennerkette überein...*/
+            der Trennerkette an irgendeiner Stelle überein...*/
             if (zeichenkette.charAt(position) == trennerkette.charAt(
                     trennzeichenAnPosition)) {
                 /*...wird das Semaphor auf 1 gesetzt.*/
                 semaphor = 1;
             }
             /*Die Position des Trennzeichens in der Trennerkette 
-            wird um 1 erhöht, damit durch die Trennzeichenkette 
-            gewandert werden kann und das nächste Trennzeichen dran kommt.*/
+            wird um 1 erhöht.*/
             trennzeichenAnPosition = trennzeichenAnPosition + 1;
         }
         /*Steht das Semaphor bei 1...*/
@@ -116,12 +81,11 @@ public class Texttrenner {
     }
     /**
      * Diese Methode liefert genau dann true, wenn der Texttrenner nach den 
-     * bisher gelieferten Wörtern, noch mindestens ein weiteres Wort enthält. 
+     * bisher gelieferten Wörtern noch mindestens ein weiteres Wort enthält. 
      * Sollte ein Wort vorhanden sein, so wird wahr ausgegeben, andernfalls 
      * falsch.
      * @return Gibt den Boole'schen Wert wieder <code>true</code>, 
-     * wenn noch mindestens ein Wort in der ersten Zeichenkette 
-     * enthalten ist, sonst <code>false</code>.
+     * wenn noch mindestens ein Wort enthalten ist, sonst <code>false</code>.
      */
     public boolean hatNochWoerter() {
         //Der Zähler zählt hoch.
@@ -137,7 +101,7 @@ public class Texttrenner {
         int trennzeichenAnPosition;
         /*Ineinander verschachtelte Schleifen, die erste Schleife 
         läuft die gesamte Zeichenkette ab...*/
-        while (zeichenAnPosition < maxPosition) {
+        while (zeichenAnPosition < zeichenkette.length()) {
             /*Das Trennzeichen wird auf die erste Position der 
             Trennerkette gesetzt.*/
             trennzeichenAnPosition = 0;
@@ -157,9 +121,9 @@ public class Texttrenner {
                 trennzeichenAnPosition = trennzeichenAnPosition + 1;
             }
             /*Ist der Wert des Zählers größer oder gleich der Länge 
-            der Trennerkette, gibt es kein Trennzeichen mehr...*/
+            der Trennerkette, gibt es kein Trennzeichen mehr 
+            und alles was in der Zeichenkette steht ist ein Wort.*/
             if (zaehler >= trennerkette.length()) {
-                /*...und alles was in der Zeichenkette steht, ist ein Wort.*/
                 hatNochWoerter = true;
                 //Das Zeichen wird auf das Ende der Zeichenkette gesetzt.
                 zeichenAnPosition = zeichenkette.length() - 1;
@@ -167,7 +131,7 @@ public class Texttrenner {
             //Das Zeichen wird um 1 erhöht.
             zeichenAnPosition = zeichenAnPosition + 1;
         }
-        //Die Ausgabe erfolgt, je nach Fall mit wahr oder falsch.
+        //Die Ausgabe erfolgt je nach Fall mit wahr oder falsch.
         return hatNochWoerter;
     }
     /**
@@ -177,34 +141,32 @@ public class Texttrenner {
      */
     private int anfangWort() {
         /*Solange ein Trennzeichen vorliegt, es noch Wörter gibt 
-        und die Position noch nicht am Ende der Zeichenkette angelangt ist,...*/
-        while (istTrennzeichen() && hatNochWoerter()
-                && position < maxPosition) {
-            /*...wird die Position jeweils um 1 hochgezählt (inkrementiert). 
-            Hiermit wird die Methode trennzeichenUeberlaeufer() eingespart.*/
+        und die Position noch nicht am Ende der Zeichenkette angelangt ist, 
+        wird die Position jeweils um 1 hochgezählt (inkrementiert).*/
+        while (istTrennzeichen() && hatNochWoerter() 
+                && position < zeichenkette.length()) {
             position = position + 1;
         }
-        //Gibt jeweils die aktuellste Position zurück.
         return position;
     }
     /**
      * Diese Methode liefert das nächste Wort aus der zugrunde liegenden 
-     * Zeichenkette. 
+     * Zeichenkette.
      * Falls die Zeichenketteke kein Wort mehr enthält, 
      * liefert die Methode null.
      * @return Liefert das nächste Wort aus der Zeichenkette.
      */
     public String gibNaechstesWort() {
-        //Zu Anfang wird die Teilzeichenkette (Tokenizer), ohne Wert erstellt.
+        //Zu Anfang wird die Teilzeichenkette (Tokenizer) ohne Wert erstellt.
         String teilzeichenkette = "";
         //Gibt es kein zu lieferndes Wort mehr, bekommt sie den Wert "null".
         if (!hatNochWoerter()) {
             teilzeichenkette = null;
-        } 
+        }
         /*Solange die Position kleiner oder gleich der Länge der Zeichenkette 
         ist und kein Trennzeichen vorkommt...*/
-        while (position <= zeichenkette.length() && !istTrennzeichen()
-                && position < maxPosition) {
+        while (position <= zeichenkette.length() && !istTrennzeichen() 
+                && position < zeichenkette.length()) {
             /*...wird das Zeichen der Position, der Teilzeichenkette 
             hinzugefügt.*/
             teilzeichenkette = teilzeichenkette + zeichenkette.charAt(position);
